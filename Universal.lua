d6c00d93eb73c38bf1ff475d1f0510cb6d3a1984
Hristow20 <92942468+Hristow20@users.noreply.github.com>
2026-07-05 03:19:05 +0300

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -35,7 +35,7 @@ local function GetCharacter(player)
 end
 
 local Window = Parvus.Utilities.UI:Window({
-    Name = ("Parvus Hub %s %s"):format(utf8.char(8212), "Parvus.Game.Name"),
+    Name = ("Parvus Hub %s %s"):format(utf8.char(8212), Parvus.Game.Name),
     Position = UDim2.new(0.5, -248 * 3, 0.5, -248)
 }) do
 
